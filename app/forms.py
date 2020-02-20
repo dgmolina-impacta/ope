63c5424a2e6336914fb92d80e78362f67ef3e45c
caiovillar <caiovillar@Caios-MacBook.local>
2020-02-20 08:23:46 -0300

    Telas de login, cadastro de usuário e cliente

---
+++ app/forms.py
@@ -0,0 +1,68 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', 
+                    validators=[DataRequired(), Length(min=2, max=20)])
+    
+    email = StringField('Email', 
+                    validators=[DataRequired(), Email()])
+
+    password = PasswordField('Senha', 
+                    validators=[DataRequired()])
+
+    confirm_password = PasswordField('Confirmar Senha', 
+                    validators=[DataRequired(), EqualTo('senha')])
+
+    submit = SubmitField('Cadastrar')
+
+
+class LoginForm(FlaskForm):
+    
+    email = StringField('Email', 
+                    validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password', 
+                    validators=[DataRequired()])
+
+    remember = BooleanField('Remember Me')
+
+    submit = SubmitField('Login')
+
+
+class ClientForm(FlaskForm):
+    cpf = StringField('CPF', 
+                    validators=[DataRequired(), Length(min=11, max=11)])
+    
+    name = StringField('Nome', 
+                    validators=[DataRequired(), Length(min=2, max=100)])
+    
+    phone = StringField('Tel. Fixo')
+
+    mobile = StringField('Celular', 
+                    validators=[DataRequired()])
+
+    email = StringField('Email', 
+                    validators=[DataRequired(), Email()])
+
+    cep = StringField('CEP', 
+                    validators=[DataRequired()])
+    
+    address = StringField('Endereço', 
+                    validators=[DataRequired()])
+
+    number = StringField('Número', 
+                    validators=[DataRequired()])
+    
+    complement = StringField('Complemento')
+
+    city = StringField('Cidade', 
+                    validators=[DataRequired()])
+    
+    state = StringField('Estado', 
+                    validators=[DataRequired()])
+
+    submit = SubmitField('Cadastrar')
+
--- app/osmanager.py
+++ app/osmanager.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm, ClientForm
 app = Flask(__name__)
+
+app.config ['SECRET_KEY'] = '732c3eeec9fb75e61d6edc6458d2faad'
 
 os = {"numero": "xxxxx",
     "cliente": "Daniel Gomes Molina",
@@ -12,9 +15,24 @@ os = {"numero": "xxxxx",
 def home():
     return render_template("home.html", os=os)
 
-@app.route('/about')
-def about():
-    return "<h1>About Page</h1>"
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Conta Criada para {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template("register.html", title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template("login.html", title='Login', form=form)
+
+@app.route('/client', methods=['GET', 'POST'])
+def client():
+    form = ClientForm()
+    return render_template("client.html", title='Novo Cliente', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
