0098a365cf1f5600eb4d98ee5fb216885d1cb261
dgmolina-impacta <daniel.molina@aluno.faculdadeimpacta.com.br>
2020-05-14 23:08:16 -0300

    Páginas de erros

--- app/osmanager/__init__.py
+++ app/osmanager/__init__.py
@@ -22,8 +22,10 @@ def create_app(config_class=Config):
     from osmanager.clientes.routes import clientes
     from osmanager.main.routes import main
     from osmanager.ordens.routes import ordens
+    from osmanager.erros.handlers import erros
     app.register_blueprint(clientes)
     app.register_blueprint(main)
     app.register_blueprint(ordens)
+    app.register_blueprint(erros)
 
     return app
