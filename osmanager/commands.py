7dce4ca6e7681ac98f4fea2e820bad9625a338d4
dgmolina-impacta <daniel.molina@aluno.faculdadeimpacta.com.br>
2020-05-16 16:14:15 -0300

    Adicionando o postgress e comandos para sua inicialização

---
+++ osmanager/commands.py
@@ -0,0 +1,14 @@
+import click
+from flask.cli import with_appcontext
+
+from osmanager import db
+
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+    db.create_all()
+
+@click.command(name='drop_tables')
+@with_appcontext
+def drop_tables():
+    db.drop_all()
